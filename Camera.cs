c22def70a1cde5bd2673135a7c3100c874d7dfde
nek0d3r <darklink.perry@gmail.com>
2023-12-02 14:41:52 -0500

    MonoGame.Extended Migration Megabranch (#20)

---
+++ Camera.cs
@@ -0,0 +1,77 @@
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Input;
+using MonoGame.Extended;
+using MonoGame.Extended.Tiled;
+using MonoGame.Extended.ViewportAdapters;
+
+public static class Camera
+{
+    private static OrthographicCamera _camera;
+    private static Vector2 _cameraPosition;
+    public static Matrix ViewMatrix { get => _camera.GetViewMatrix(); }
+
+    public static void Initialize(BoxingViewportAdapter boxingViewportAdapter)
+    {
+        _camera = new OrthographicCamera(boxingViewportAdapter);
+        _cameraPosition = new Vector2(
+            TileRender.DEFAULT_WINDOW_SIZE.X / 2,
+            TileRender.DEFAULT_WINDOW_SIZE.Y / 2
+        );
+    }
+    public static Vector2 GetMovementDirection()
+    {
+        var movementDirection = Vector2.Zero;
+        var state = Keyboard.GetState();
+        if (state.IsKeyDown(Keybindings.Down))
+        {
+            movementDirection += Vector2.UnitY;
+        }
+        if (state.IsKeyDown(Keybindings.Up))
+        {
+            movementDirection -= Vector2.UnitY;
+        }
+        if (state.IsKeyDown(Keybindings.Left))
+        {
+            movementDirection -= Vector2.UnitX;
+        }
+        if (state.IsKeyDown(Keybindings.Right))
+        {
+            movementDirection += Vector2.UnitX;
+        }
+        
+        // Can't normalize the zero vector so test for it before normalizing
+        if (movementDirection != Vector2.Zero)
+        {
+            movementDirection.Normalize(); 
+        }
+        
+        return movementDirection;
+    }
+
+    public static void MoveCamera(GameTime gameTime, TiledMap map)
+    {
+        var speed = 200;
+        var seconds = gameTime.GetElapsedSeconds();
+        var movementDirection = GetMovementDirection();
+        _cameraPosition += speed * movementDirection * seconds;
+
+        if (_cameraPosition.X < TileRender.BUFFER_SIZE.X / 2)
+        {
+            _cameraPosition.X = TileRender.BUFFER_SIZE.X / 2;
+        }
+        if (_cameraPosition.Y < TileRender.BUFFER_SIZE.Y / 2)
+        {
+            _cameraPosition.Y = TileRender.BUFFER_SIZE.Y / 2;
+        }
+        if (_cameraPosition.X > map.WidthInPixels - TileRender.BUFFER_SIZE.X / 2)
+        {
+            _cameraPosition.X = map.WidthInPixels - TileRender.BUFFER_SIZE.X / 2;
+        }
+        if (_cameraPosition.Y > map.HeightInPixels - TileRender.BUFFER_SIZE.Y / 2)
+        {
+            _cameraPosition.Y = map.HeightInPixels - TileRender.BUFFER_SIZE.Y / 2;
+        }
+
+        _camera.LookAt(_cameraPosition);
+    }
+}
--- John.cs
+++ John.cs
@@ -1,8 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using Microsoft.Xna.Framework;
+﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
+using MonoGame.Extended.Tiled;
+using MonoGame.Extended.Tiled.Renderers;
+using MonoGame.Extended.ViewportAdapters;
 
 namespace MonoGameJam5;
 
@@ -13,13 +14,9 @@ public class John : Game
     private RenderTarget2D _render;
     
     KeyboardState currentKey = new KeyboardState(), prevKey;
-    
-    // Test tilemap
-    Map testMap;
 
-    // Test sprite
-    Texture2D testTile;
-    Texture2D testConveyor;
+    TiledMap _tiledMap;
+    TiledMapRenderer _tiledMapRenderer;
 
     public John()
     {
@@ -34,18 +31,21 @@ public class John : Game
 
     protected override void Initialize()
     {
-        // TODO: The map will need to be bigger than the viewport eventually.
-        // We will make changes here when that happens.
-        testMap = new Map(TileRender.BUFFER_TILE_DIMS);
-
-        base.Initialize();
-
         // Set buffer size to default
         _graphics.PreferredBackBufferWidth = TileRender.DEFAULT_WINDOW_SIZE.X;
         _graphics.PreferredBackBufferHeight = TileRender.DEFAULT_WINDOW_SIZE.Y;
         _graphics.ApplyChanges();
 
         TileRender.WindowChanged(Window, null);
+
+        Camera.Initialize(new BoxingViewportAdapter(
+            Window,
+            GraphicsDevice,
+            TileRender.DEFAULT_WINDOW_SIZE.X,
+            TileRender.DEFAULT_WINDOW_SIZE.Y
+        ));
+        
+        base.Initialize();
     }
 
     protected override void LoadContent()
@@ -53,25 +53,8 @@ public class John : Game
         _spriteBatch = new SpriteBatch(GraphicsDevice);
         _render = new RenderTarget2D(GraphicsDevice, TileRender.BUFFER_SIZE.X, TileRender.BUFFER_SIZE.Y);
 
-        testTile = Content.Load<Texture2D>("test");
-        testConveyor = Content.Load<Texture2D>("pixel/boxsadnarrow");
-
-        for(var y = 0; y < TileRender.BUFFER_TILE_DIMS.Y; y++)
-        {
-            for(var x = 0; x < TileRender.BUFFER_TILE_DIMS.X; x++)
-            {
-                testMap.AddToMap(new TestTile
-                {
-                    Image = testTile
-                }, x, y, Tile.TileLayer.Floor);
-                // For testing, one in 8 tiles will now also have a test conveyor
-                if ((Random.Shared.Next() & 0x111) == 1)
-                {
-                    testMap.AddToMap(new Conveyor(testConveyor, (Random.Shared.Next() & 1) == 1, Conveyor.Direction.North, Random.Shared.Next() % 13),
-                        x, y, Tile.TileLayer.Active);
-                }
-            }
-        }
+        _tiledMap = Content.Load<TiledMap>("testmap");
+        _tiledMapRenderer = new TiledMapRenderer(GraphicsDevice, _tiledMap);
     }
 
     protected override void Update(GameTime gameTime)
@@ -80,9 +63,11 @@ public class John : Game
         prevKey = currentKey;
         currentKey = Keyboard.GetState();
 
-        // Default exit on escape or back on Xbox controller
-        if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+        // Default exit on escape
+        if (currentKey.IsKeyDown(Keybindings.Exit))
+        {
             Exit();
+        }
 
         // Window is not supporting Alt + Enter despite Window.AllowAltF4, so this handles shortcut fullscreen toggle
         if ((currentKey.IsKeyDown(Keys.LeftAlt) || currentKey.IsKeyDown(Keys.RightAlt)) && currentKey.IsKeyDown(Keys.Enter) && !prevKey.IsKeyDown(Keys.Enter))
@@ -110,6 +95,10 @@ public class John : Game
             TileRender.WindowChanged(Window, null);
         }
 
+        _tiledMapRenderer.Update(gameTime);
+
+        Camera.MoveCamera(gameTime, _tiledMap);
+
         base.Update(gameTime);
     }
 
@@ -119,23 +108,7 @@ public class John : Game
         GraphicsDevice.SetRenderTarget(_render);
         GraphicsDevice.Clear(Color.Black);
 
-        // Drawing begins here
-        _spriteBatch.Begin(SpriteSortMode.Immediate, BlendState.AlphaBlend, SamplerState.PointClamp);
-
-        for (int l = 0; l < (int)Tile.TileLayer.NumLayers; ++l)
-        {
-            foreach (Map.TilePos t in testMap.map[l])
-            {
-                _spriteBatch.Draw(
-                    t.tile.Image,
-                    new Rectangle(t.loc.X * TileRender.TILE_SIZE, t.loc.Y * TileRender.TILE_SIZE, TileRender.TILE_SIZE, TileRender.TILE_SIZE),
-                    new Rectangle(TileRender.TILE_SIZE * t.tile.X, TileRender.TILE_SIZE * t.tile.Y, TileRender.TILE_SIZE, TileRender.TILE_SIZE),
-                    Color.White
-                );
-            }
-        }
-
-        _spriteBatch.End();
+        _tiledMapRenderer.Draw(Camera.ViewMatrix);
 
         // Set render target to device back buffer and clear
         GraphicsDevice.SetRenderTarget(null);
@@ -143,7 +116,7 @@ public class John : Game
 
         // Draw sprite buffer to back buffer
         _spriteBatch.Begin(SpriteSortMode.Immediate, BlendState.AlphaBlend, SamplerState.PointClamp);
-        _spriteBatch.Draw((Texture2D)_render, TileRender.renderDims, Color.White);
+        _spriteBatch.Draw(_render, TileRender.renderDims, Color.White);
         _spriteBatch.End();
 
         base.Draw(gameTime);
