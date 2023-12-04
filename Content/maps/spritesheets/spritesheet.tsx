93066f9bc707e64c6347bb37f9f789340261225d
nek0d3r <darklink.perry@gmail.com>
2023-12-04 18:44:32 -0500

    organized map resources

---
+++ Content/maps/spritesheets/spritesheet.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="spritesheet" tilewidth="24" tileheight="24" tilecount="91" columns="7">
+ <image source="../../pixel/spritesheet.png" width="168" height="312"/>
+</tileset>
